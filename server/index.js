726525e020011419ec43bec25193811cd8951c41
Miifu <mil_la2000@hotmail.com>
2022-12-02 23:35:20 +0200

    V5 graafi ja tiedot valmiit

--- server/index.js
+++ server/index.js
@@ -97,7 +97,18 @@ app.get('/v2data', async (req, res) => {
   }
 })
  
-  
+app.get('/V5data', async (req, res) => {
+  try{
+    const connection = await mysql.createConnection(config.db)
+    const [result,] = await connection.execute('select * from v5data')
+
+    if(!result) result=[]
+    res.status(200).json(result)
+  }catch(err){
+    res.status(500).json({error: err.message})
+  }
+})
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import 'chartjs-adapter-luxon';
 
+
 function App() {
   const [userDataChart, setUserData] = useState();
+  const [v5Data, setV5Data] = useState();
+
 
 
  
@@ -163,6 +166,36 @@ function App() {
     getV1data()
   }, [])
 
+  useEffect(() => {
+    async function getV5data(){
+      const responseV5Data = await axios.get('http://localhost:3001/v5data')
+      console.log(responseV5Data)
+      setV5Data({
+        datasets: [
+          {
+            label: "VostokIceCore",
+            data: responseV5Data.data, //UserData.map((data) => data.userGain)
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0",
+            ],
+            borderColor: "black",
+            borderWidth: 1,
+            parsing: {
+              xAxisKey: "time",
+              yAxisKey: "co2"
+            },
+          },        
+        ],
+      })
+    }
+    getV5data()
+  }, [])
+
+
   const options = {
     responsive: true,
     plugins: {
@@ -183,6 +216,28 @@ function App() {
     },
   };
 
+  const options2 = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Historical CO2 record from the Vostok ice core",
+      },
+    },
+    scales: {
+      x: {
+        type: "linear",
+        min: 0,
+        max: 419200,
+        display: true,
+        position: "right",
+      },
+    },
+  };
+
   if(userDataChart === undefined){
     return null;
   }
@@ -192,7 +247,16 @@ function App() {
     <div className="App">
       <div style={{ width: 1200 }}>
         <Line options={options} data={userDataChart} />
-      </div>        
+        <h3>V5 Historical CO2 record from the Vostok ice core</h3>
+        <Line options={options2} data={v5Data} />
+        <br></br>
+        <p><a href="https://cdiac.ess-dive.lbl.gov/trends/co2/vostok.html">V5 description</a><br />
+        <a href="https://cdiac.ess-dive.lbl.gov/ftp/trends/co2/vostok.icecore.co2">V5 dataset source</a></p>
+        <p>
+        V5 chart shows data of Soviet Co2 levels study from Vostok research center located at Antarctica.<br />
+        Data was gathered from deep ice bore holes. Samples contain roughly 400000 years worth of data.
+        </p>
+      </div>
     </div>
   );
 }
